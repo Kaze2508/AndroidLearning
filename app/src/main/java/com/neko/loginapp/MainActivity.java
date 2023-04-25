2485aa0005bb99bbf857dc3e9d2e20d04e6c3518
Kaze Phake <phongsgvn@gmail.com>
2023-04-26 01:35:45 +0700

    Updated Finished Demo

--- app/src/main/java/com/neko/loginapp/MainActivity.java
+++ app/src/main/java/com/neko/loginapp/MainActivity.java
@@ -3,6 +3,13 @@ package com.neko.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(MainActivity.this, "LOGIN FAIL!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
